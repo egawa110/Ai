7b71d201188fec0823e4441248e2a3fcd9e73894
DESKTOP-OH5B7IO\GA2B <h1202416002@hiratagakuen.onmicrosoft.com>
2025-10-28 16:35:11 +0900

    no message

--- AI課題/Assets/Script/Skill Cooldown.cs
+++ AI課題/Assets/Script/Skill Cooldown.cs
@@ -6,18 +6,14 @@ using UnityEngine;
 public class SkillCooldown : MonoBehaviour
 {
     public TextMeshProUGUI countdownText;  //テキスト
-    public TextMeshProUGUI count_sText;  //テキスト
 
     public float time = 0f;
     //時間停止
     public float count = 5f;
     private bool FLAG = false;
     //スピードUP
-    public float count_s = 5f;
-    public bool FLAG2 = false;
     //参照
     public GamePauseController GP;
-    public Skill_Speed S_SP;
 
     void Update()
     {
@@ -36,24 +32,6 @@ public class SkillCooldown : MonoBehaviour
                 FLAG = false;
             }
         }
-
-        if (S_SP.FLAG_S)
-        {
-            time = count_s;
-            FLAG2 = true;
-        }
-        if (FLAG2)
-        {
-            time -= Time.deltaTime;
-            count_sText.text = Mathf.Ceil(time).ToString() + "";
-            if (time <= 0f)
-            {
-                count_sText.text = ""; // 表示を消す
-                FLAG2 = false;
-                S_SP.FLAG_S = false;
-            }
-        }
-
     }
 
 }
--- AI課題/Assets/Script/Skill_Speed.cs
+++ AI課題/Assets/Script/Skill_Speed.cs
@@ -1,16 +1,27 @@
+using TMPro;
 using UnityEngine;
 
 public class Skill_Speed : MonoBehaviour
 {
     public float Speed = 5f;
     public float OrigineSpeed = 0f;
-    public bool FLAG_S = false;
-    public PlayerController controller;
-    public SkillCooldown SK_D;
+    public float time = 0f;
+    public float count = 5f;
 
-    void Update()
+    public bool FLAG   = false;
+    public bool FLAG2  = false;
+    public bool FLAG_S = false;
+
+    public TextMeshProUGUI count_sText;  //テキスト
+    public PlayerController controller;
+
+    void Start()
     {
         OrigineSpeed = controller.moveSpeed;
+
+    }
+    void Update()
+    {
         if(Input.GetKeyDown(KeyCode.R))
         {
             if (!FLAG_S)
@@ -19,9 +30,27 @@ public class Skill_Speed : MonoBehaviour
             }
             FLAG_S = true;
         }
-        if(!SK_D.FLAG2)
+        if(FLAG2)
         {
             controller.moveSpeed = OrigineSpeed;
         }
+
+        //クールダウン処理
+        if (FLAG_S)
+        {
+            time = count;
+        }
+        if (FLAG)
+        {
+            time -= Time.deltaTime;
+            count_sText.text = Mathf.Ceil(time).ToString() + "";
+            if (time <= 0f)
+            {
+                count_sText.text = ""; // 表示を消す
+                FLAG = false;
+                FLAG_S = false;
+            }
+        }
+
     }
 }
